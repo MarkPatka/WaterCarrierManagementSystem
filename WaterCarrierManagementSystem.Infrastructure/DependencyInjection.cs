a33ffb99720dc637dfbed3fc7fdff574df83f946
MarkPatka <mark.kaz.1998@gmail.com>
2025-06-10 16:27:39 +0300

    Scaffolds Persistence Registering

--- WaterCarrierManagementSystem.Infrastructure/DependencyInjection.cs
+++ WaterCarrierManagementSystem.Infrastructure/DependencyInjection.cs
@@ -1,4 +1,16 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using NHibernate;
+using System.Configuration.Internal;
+using System.Reflection;
+using WaterCarrierManagementSystem.Application.Common.Persistence;
+using WaterCarrierManagementSystem.Application.Common.Persistence.Repositories;
+using WaterCarrierManagementSystem.Infrastructure.Persistence;
+using WaterCarrierManagementSystem.Infrastructure.Persistence.Configurations;
+using WaterCarrierManagementSystem.Infrastructure.Persistence.Repositories;
 
 namespace WaterCarrierManagementSystem.Infrastructure;
 
@@ -6,8 +18,57 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services)
     {
+        services
+            .RegisterRepositories()
+            .RegisterNHibernate()
+            ;
 
         return services;
     }
 
+    private static IServiceCollection RegisterNHibernate(this IServiceCollection services)
+    {
+        services.AddSingleton<ISessionFactory>(provider =>
+        {
+            var dbSettings = provider.GetRequiredService<IOptions<DatabaseSettings>>().Value;
+
+            return Fluently.Configure()
+                .Database(MySQLConfiguration.Standard
+                    .ConnectionString(dbSettings.ConnectionString)
+                    .AdoNetBatchSize(20)
+                )
+                .Mappings(m => m.FluentMappings.AddFromAssembly(Assembly.GetExecutingAssembly()))
+                .ExposeConfiguration(cfg =>
+                {
+                    cfg.SetProperty(NHibernate.Cfg.Environment.GenerateStatistics, "false");
+                    cfg.SetProperty(NHibernate.Cfg.Environment.PrepareSql, "true");
+                })
+                .BuildSessionFactory();
+        });
+
+        services.AddScoped<ISession>(provider =>
+        {
+            var sessionFactory = provider
+                .GetRequiredService<ISessionFactory>();
+
+            var session = sessionFactory.OpenSession();
+
+            session.FlushMode = FlushMode.Commit;
+
+            return session;
+        });
+
+        return services;
+    }
+
+    private static IServiceCollection RegisterRepositories(this IServiceCollection services)
+    {
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+        services.AddScoped<IRepositoryFactory, RepositoryFactory>();
+        services.AddScoped<IOrderRepository, OrderRepository>();
+        services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+        services.AddScoped<IContractorRepository, ContractorRepository>();
+
+        return services;
+    }
 }

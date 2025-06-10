d9a7ce7ef87aee7a92bc21be872768e60ba9798f
MarkPatka <mark.kaz.1998@gmail.com>
2025-06-10 16:26:16 +0300

    Fix INN Domain Entity

--- WaterCarrierManagementSystem.Domain/ContractorAggregate/Entities/INN.cs
+++ WaterCarrierManagementSystem.Domain/ContractorAggregate/Entities/INN.cs
@@ -5,8 +5,11 @@ namespace WaterCarrierManagementSystem.Domain.ContractorAggregate.Entities;
 
 public class INN : Entity<InnId>
 {
+    public Int64 Number { get; }
+
     private INN() { }
-    private INN(InnId inn) : base(inn) { }
-    public static INN Create(int innNumber) =>
-        new(InnId.Create(innNumber));
+
+    private INN(InnId inn, Int64 number) : base(inn) { Number = number; }
+    public static INN Create(int innId, long innNumber) =>
+        new(InnId.Create(innId), innNumber);
 }

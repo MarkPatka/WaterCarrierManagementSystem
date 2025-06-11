c25e70892cbd2f3e8ec4074805ead86df9636ffd
Mark Patka <mark.kaz.1998@gmail.com>
2025-06-11 20:17:52 +0300

    Adjusts The Base Repository Interface With The Methods of Generic Repository

--- WaterCarrierManagementSystem.Application/Common/Persistence/IRepository.cs
+++ WaterCarrierManagementSystem.Application/Common/Persistence/IRepository.cs
@@ -1,7 +1,15 @@
-﻿namespace WaterCarrierManagementSystem.Application.Common.Persistence;
+﻿using System.Linq.Expressions;
+using WaterCarrierManagementSystem.Domain.Common.Abstract;
+
+namespace WaterCarrierManagementSystem.Application.Common.Persistence;
 
 public interface IRepository<T> where T : class
 {
+    public Task<IList<T>> GetAll();
+    public Task<IList<T>> GetAll(Expression<Func<T, bool>> predicate);
+    public Task<IList<T>> GetAll(int pageIndex, int pageSize);
+
+
     public Task<T?> GetByIdAsync(object id,  CancellationToken cancellationToken = default);
     public Task CreateAsync(T entity, CancellationToken cancellationToken = default);
     public Task UpdateAsync(T entity, CancellationToken cancellationToken = default);
